a04e7f1e0f99b3d18631f24f7aabd5e94fe2c676
Lwam Tekie <ltekie87@yahoo.com>
2019-03-11 20:32:49 -0500

    added index.html main.css man.js

--- main.js
+++ main.js
@@ -1,1 +1,91 @@
-console.log("setup pie");
+const pies = [
+        {
+          name: 'Dutch Apple Pie',
+          ingredients: 'apples,sugar,butter,nutmeg,dutch people',
+          bakeTemp: 5000,
+          drinkPairing: 'Earl Grey Tea',
+          imageUrl: 'https://search.chow.com/thumbnail/800/0/www.chowstatic.com/assets/2013/09/30859_Recipeimage_620x413_dutch_apple_pie.jpg',
+          instructor: 'Zoe',
+          iceCream: 'Vanilla',
+        },
+        {
+          name: 'Berry Pie',
+          ingredients: 'berries',
+          bakeTemp: 400,
+          drinkPairing: 'wine',
+          imageUrl: 'https://tastesbetterfromscratch.com/wp-content/uploads/2015/11/Triple_Berry_Pie8.jpg',
+          instructor: 'Zoe',
+          iceCream: 'banana',
+        },
+        {
+          name: 'Pumpkin Pie',
+          ingredients: 'pumpkins, nutmeg, cinnamon, graham crackers, pilgrims',
+          bakeTemp: 42,
+          drinkPairing: 'egg nog',
+          imageUrl: 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/1033249144001/22a0c25d-2fee-415c-a4e7-91d008e276bb/a904f3e2-3cd9-40d6-ace9-f8dbd2d616cd/1280x720/match/image.jpg',
+          instructor: 'Michael',
+          iceCream: 'Vanilla',
+        },
+        {
+          name: 'Shoo Fly Pie',
+          ingredients: 'Molasses, nutmeg, cinnamon, butter, graham cracker ',
+          bakeTemp: 1234,
+          drinkPairing: 'Apple Cider',
+          imageUrl: 'https://static01.nyt.com/images/2016/09/27/dining/27COOKING-SHOOFLY-PIE2/27COOKING-SHOOFLY-PIE2-articleLarge.jpg',
+          instructor: 'Michael',
+          iceCream: 'Coffee',
+        },
+        {
+          name: 'Pecan Pie',
+          ingredients: 'Pecans, sugar, butter, flour',
+          bakeTemp: 5000,
+          drinkPairing: 'Milk',
+          imageUrl: 'https://cookiesandcups.com/wp-content/uploads/2018/10/pecanpie-3.jpg',
+          instructor: 'Saul',
+          iceCream: 'Vanilla',
+        },
+        {
+          name:'Keylime Pie',
+          ingredients: 'lemons, sugar, butter, flour',
+          bakeTemp: 5000,
+          drinkPairing: 'Water',
+          imageUrl: 'https://www.williams-sonoma.com/wsimgs/rk/images/dp/recipe/201851/0020/img38l.jpg',
+          instructor: 'Saul',
+          iceCream: 'none',
+        },
+      ];
+
+
+
+const printToDom = (divID, textToPrint) => {
+ const selectedDiv= document. getElementById(divID)
+ selectedDiv.innerHTML = textToPrint;
+};
+const pieBuilder = (pies) =>{
+let domString = '';
+    forEach(let i = 0; i < pie.length; i++) {
+        domString += `<div class= "pie-ingredints">`
+        domString += `<h4>${pie[i].name}</h4>`;
+        domString += `<p>${pie[i].bakeTemp}</p>`;
+        domString += `<p>${pie[i].instructor}</p>`;
+        domString += `</div>`;
+    };
+    
+    printToDom('pies', domString);
+};
+
+
+const piename = () => {
+    let domstring = '';
+    pies.forEach((pies) => {
+        domstring += `<h2>${pies.name}</h2>`;
+    
+    });
+};
+    
+
+const init = () => {
+    pieBuilder();
+    console.log(piename);
+};
+init();
